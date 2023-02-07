6bce7eebea4f76320621efbae2088f2d3f99ed08
Oded <odedatias8115@gmail.com>
2023-02-07 03:44:52 +0200

    Adding the navigation between the screens

--- App.js
+++ App.js
@@ -1,24 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-// RN Core components
-import { StyleSheet, Text, View } from 'react-native';
+// Navigation imports
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// Custom components imports
+import ManageExpense from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+
+// Navigators initiallization
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+// BottomTab component (The nested navigator component)
+const ExpensesOverview = () => {
+	// ExpensesOverview render
+	return (
+		<BottomTabs.Navigator>
+			{/* First Screen - Recent Expenses */}
+			<BottomTabs.Screen name='RecentExpenses' component={RecentExpenses} />
+			{/* Second Screen - All Expenses */}
+			<BottomTabs.Screen name='AllExpenses' component={AllExpenses} />
+		</BottomTabs.Navigator>
+	);
+};
 
 // App Component
 export default function App() {
 	// App Render
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+		<>
 			<StatusBar style='auto' />
-		</View>
+			{/* Navigation configurations */}
+			<NavigationContainer>
+				{/* The main navigator */}
+				<Stack.Navigator>
+					{/* First Screen - Nested BottomTab Navigator*/}
+					<Stack.Screen name='ExpensesOverview' component={ExpensesOverview} />
+					{/* Second Screen - ManageExpense */}
+					<Stack.Screen name='ManageExpense' component={ManageExpense} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</>
 	);
 }
-
-// App StyleSheet
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
