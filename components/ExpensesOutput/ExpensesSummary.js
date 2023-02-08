d77bf41420bf6d84af3772a31f87f2af742d533e
Oded <odedatias8115@gmail.com>
2023-02-08 08:30:15 +0200

    Adding style to ExpensesSummary component

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,5 +1,7 @@
 // RN Core components
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+// Constants
+import { GlobalStyles } from './../../constants/styles';
 
 // ExpensesSummary component
 const ExpensesSummary = ({ expenses, periodName }) => {
@@ -11,11 +13,33 @@ const ExpensesSummary = ({ expenses, periodName }) => {
 
 	// ExpensesSummary render
 	return (
-		<View>
-			<Text>{periodName}</Text>
-			<Text>{expensesSum.toFixed(2)}$</Text>
+		<View style={styles.container}>
+			<Text style={styles.periodName}>{periodName}</Text>
+			<Text style={styles.expensesSum}>{expensesSum.toFixed(2)}$</Text>
 		</View>
 	);
 };
+
+// ExpensesSummary StyleSheet
+const styles = StyleSheet.create({
+	container: {
+		padding: 8,
+		backgroundColor: GlobalStyles.colors.primary50,
+		borderRadius: 6,
+		// To flex the items side by side with space between them
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	periodName: {
+		fontSize: 12,
+		color: GlobalStyles.colors.primary400,
+	},
+	expensesSum: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		color: GlobalStyles.colors.primary500,
+	},
+});
 
 export default ExpensesSummary;
